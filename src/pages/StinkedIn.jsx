ec205e4da29ac670ba08c38af5564b16d652cd9e
BhagyaPasawala <bhagya1710@gmail.com>
2024-09-27 16:14:38 -0400

    some commits

--- src/pages/StinkedIn.jsx
+++ src/pages/StinkedIn.jsx
@@ -4,7 +4,7 @@ export default function StinkedIn() {
             <div className="wired-container">
                 <div className="wired-title">
                     <h1>
-                    ➸ StinkedIn
+                    ➸ Job Portal
                     </h1>
                     <p>
                         Job Portal parody
@@ -23,26 +23,23 @@ export default function StinkedIn() {
                     <div className="wired-context">
                     
                         <h1>➸Context</h1>
-                        <p> StinkedIn is the anti-networking platform you never knew you needed!
-                            A parody of the corporate social network. Instead of showing off polished résumés and
-                            endless success stories, here you can embrace your inner chaos and laugh at the struggles
-                            of the modern job hunt. Whether you're a sleep-deprived coder, an overworked intern, or just
-                            tired of “networking”, StinkedIn is your space to vent, joke, and connect with others who
-                            understand that sometimes.. things just stink! 😄
+                        <p> This is the anti-networking platform you never knew you needed!
+                            A parody of the corporate social network. Instead of showing off
+                            polished résumés and endless success stories, here you can embrace
+                            your inner chaos and laugh at the struggles of the modern job hunt.
+                            Whether you're a sleep-deprived coder, an overworked intern, or just tired of “networking”,
+                            This is your space to vent, joke, and connect with others who understand that sometimes.. things just suck! 😄
                             </p>
                         <h1>➸Process</h1>
-                        <p> Built using ReactJs, for now this is just a front-end UI project, but I am
-                            working on its backend so that anyone can post anything they want. Working on this 
-                            project was super tricky as I was learning props. Now I am going to implement messaging feature 
-                            and fake job portal too.
+                        <p> Built using ReactJs, for now, this is just a front-end UI project,
+                            but I am working on its backend so that anyone can post anything they want.
                             <br />
-                            StinkedIn is a parody project created solely for fun and educational purposes.
+                            This is a parody project created solely for fun and educational purposes.
                             It is not intended for commercial use, and I do not earn any profit from this project.
-                            And this project is in no way affiliated with or endorsed by LinkedIn. 
+                            And this project is in no way affiliated with or endorsed by LinkedIn. The purpose of this project
+                            is to practice and showcase my coding skills, and no infringement is intended.
                             <br />
                             <br />
-                            The purpose of StinkedIn is
-                            to practice and showcase my coding skills, and no infringement is intended.
                             But still, if anyone's offended, you have my regards.
                             </p>
                             <br />
