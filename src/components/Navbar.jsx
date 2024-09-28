9e2fb641b25d63cbe34989b711af8d77b3845f04
BhagyaPasawala <bhagya1710@gmail.com>
2024-09-28 08:41:14 -0400

    resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
                 <a className='contact' href="https://www.linkedin.com/in/bhagya-pasawala-a66251188/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a className='contact' href="https://github.com/BhagyaPasawala" target="_blank" rel="noopener noreferrer">Github</a>
                 <a className='contact' href="https://www.behance.net/bhagyapasawala" target="_blank" rel="noopener noreferrer">Behance</a>
-                <a className='contact' href="/BhagyaResume.pdf" target="_blank" rel="noopener noreferrer">Resume </a>
+                <a className='contact' href="/BhagyaPasawalaResume.pdf" target="_blank" rel="noopener noreferrer">Resume </a>
                 <a className='contact' href="mailto:bhagya1710@gmail.com">Contact</a>
             </div>
             <div className='about'>
               <h1>
-              Self-taught software developer with over 3 years of experience 
+              Self-taught software developer with over 4 years of experience 
               in design, web development, <br /> and 3D. I focus on building projects 
               and video games that inspire me. Here are some of the  <br /> projects I have worked on.
               </h1>
